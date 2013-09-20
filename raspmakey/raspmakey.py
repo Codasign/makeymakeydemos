9209dab8496154035d34b7ffe8b8eb59612b2e20
Becky Stewart <becky@codasign.com>
2013-09-20 22:50:50 +0000

    Lights turning on and off with spacebar, w, and s

--- raspmakey/raspmakey.py
+++ raspmakey/raspmakey.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#! encoding: utf-8
 import RPi.GPIO as GPIO
 import sys
 import select
@@ -11,30 +13,52 @@ def isData():
 
 
 GPIO.setmode(GPIO.BCM)
-GREEN_LED = 18
-RED_LED = 23
-GPIO.setup(GREEN_LED, GPIO.OUT)
-GPIO.setup(RED_LED, GPIO.OUT)
+RED_LEDS = 15
+GREEN_LEDS = 18
+BLUE_LEDS = 14
+
+red_state = False
+green_state = False
+blue_state = False
+
+GPIO.setup(RED_LEDS, GPIO.OUT)
+GPIO.setup(GREEN_LEDS, GPIO.OUT)
+GPIO.setup(BLUE_LEDS, GPIO.OUT)
+
+GPIO.output(RED_LEDS, red_state)
+GPIO.output(GREEN_LEDS, green_state)
+GPIO.output(BLUE_LEDS, blue_state)
 
 old_settings = termios.tcgetattr(sys.stdin)
 try:
     tty.setcbreak(sys.stdin.fileno())
 
-    i = 0
-    while 1:
-        # print i
-        # i += 1
+    while True:
 
         if isData():
             c = sys.stdin.read(1)
-            if c == 'a':
-                print "on"
-		GPIO.output(GREEN_LED, True)
+	    if c == chr(32):
+		blue_state = not  blue_state
+		GPIO.output(BLUE_LEDS, blue_state)
+		print "blue"
 	    if c == 's':
-		print "off"
-		GPIO.output(GREEN_LED, False)
+		green_state = not green_state
+ 		GPIO.output(GREEN_LEDS, green_state);
+		print "green"
+
+            if c == 'w':
+                print "red"
+		red_state = not red_state
+		GPIO.output(RED_LEDS, red_state)
             if c == chr(ascii.ESC):
                 break
 
 finally:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+    GPIO.output(RED_LEDS, False)
+    GPIO.output(GREEN_LEDS, False)
+    GPIO.output(BLUE_LEDS, False)
+    GPIO.setup(RED_LEDS, GPIO.IN)
+    GPIO.setup(GREEN_LEDS, GPIO.IN)
+    GPIO.setup(BLUE_LEDS, GPIO.IN)
